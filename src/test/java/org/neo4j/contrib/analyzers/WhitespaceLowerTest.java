cb02c219df62185f633cf9b69c6e46fe1dd48e56
Stefan Armbruster <stefan@armbruster-it.de>
2020-03-29 15:58:45 +0200

    additional test for `-270°`

--- src/test/java/org/neo4j/contrib/analyzers/WhitespaceLowerTest.java
+++ src/test/java/org/neo4j/contrib/analyzers/WhitespaceLowerTest.java
@@ -5,8 +5,10 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.junit.Rule;
 import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.index.fulltext.AnalyzerProvider;
 import org.neo4j.harness.junit.Neo4jRule;
+import org.neo4j.helpers.collection.Iterators;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,7 +23,7 @@ import static org.junit.Assert.assertNotNull;
 public class WhitespaceLowerTest {
 
     @Rule
-    public Neo4jRule db = new Neo4jRule();
+    public Neo4jRule neo4j = new Neo4jRule();
 
     @Test
     public void checkTokenStream() {
@@ -31,16 +33,32 @@ public class WhitespaceLowerTest {
         Analyzer analzyer = provider.createAnalyzer();
         assertThat(analyze("CAN Open Bus Logo", analzyer), contains("can", "open", "bus", "logo"));
         assertThat(analyze("_Modbus", analzyer), contains("_modbus"));
+        assertThat(analyze("abc x-270°", analzyer), contains("abc", "x-270°"));
     }
 
     @Test
     public void checkAnalyzerIsAvailable() {
-        Map<String, Object> result =  db.getGraphDatabaseService().execute("CALL db.index.fulltext.listAvailableAnalyzers() YIELD analyzer, description RETURN analyzer, description")
+        Map<String, Object> result =  neo4j.getGraphDatabaseService().execute("CALL db.index.fulltext.listAvailableAnalyzers() YIELD analyzer, description RETURN analyzer, description")
                 .stream().filter(map -> map.get("analyzer").equals("whitespace_lower")).findFirst().orElse(null);
 
         assertNotNull(result);
         assertEquals(WhitespaceLowerAnalyzerProvider.DESCRIPTION, result.get("description"));
     }
+
+    @Test
+    public void checkSearchForTermContainingDash() {
+        GraphDatabaseService db = neo4j.getGraphDatabaseService();
+        db.execute("CALL db.index.fulltext.createNodeIndex('myIndex', ['Article'], ['title'], {analyzer: 'whitespace_lower'})");
+        db.execute( "CREATE (:Article{title:'abc x-270°'})");
+
+        assertEquals(1, Iterators.count(db.execute("CALL db.index.fulltext.queryNodes('myIndex', 'x\\\\-270°') yield node, score return node.title as text, score")));
+        assertEquals(0, Iterators.count(db.execute("CALL db.index.fulltext.queryNodes('myIndex', '\\\\-270°') yield node, score return node.title as text, score")));
+
+        List<Map<String, Object>> maps = Iterators.asList(db.execute("CALL db.index.fulltext.queryNodes('myIndex', 'x\\\\-270°') yield node, score return node.title as text, score"));
+        System.out.println(maps);
+
+    }
+
 
     private List<String> analyze(String text, Analyzer analyzer) {
         try {
